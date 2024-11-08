58aa4dc1dbc26edfcf6ea3f467d0923622d42016
iBarzha <grytcaie@gmail.com>
2024-11-08 16:00:55 +0200

    add models spots, reservations

--- parking/admin.py
+++ parking/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ParkingSpot, Reservation
 
-# Register your models here.
+@admin.register(ParkingSpot)
+class ParkingSpotAdmin(admin.ModelAdmin):
+    list_display = ('number', 'is_available', 'price_per_hour')
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'parking_spot', 'start_time', 'end_time', 'total_price')
+    list_filter = ('start_time', 'end_time')
--- parking/models.py
+++ parking/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class ParkingSpot(models.Model):
+    number = models.CharField(max_length=10, unique=True)
+    is_available = models.BooleanField(default=True)
+    price_per_hour = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"Place #{self.number} - {'Free' if self.is_available else 'Engaged'}"
+
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    parking_spot = models.ForeignKey(ParkingSpot, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    total_price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def save(self, *args, **kwargs):
+        duration = (self.end_time - self.start_time).total_seconds() / 3600
+        self.total_price = duration * self.parking_spot.price_per_hour
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Reservation #{self.id} - {self.user.username} - {self.parking_spot.number}"
